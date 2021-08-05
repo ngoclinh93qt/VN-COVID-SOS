40096bdbaa7d9469b452be96f54a87c8edd2a440
tuannguyen <huutuan345679@gmail.com>
2021-08-05 14:30:08 +0700

    navbar_sidenav_done

--- src/app/pages/container/container.component.ts
+++ src/app/pages/container/container.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-container',
   templateUrl: './container.component.html',
-  styleUrls: ['./container.component.scss']
+  styleUrls: ['./container.component.scss'],
 })
 export class ContainerComponent implements OnInit {
+  showFiller = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
