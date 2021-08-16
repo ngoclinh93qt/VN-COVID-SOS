d5e4063582f3ace60ba8e985aaa1dc3307ae2755
Thanh Ngọc Nguyễn Đức <ngochc1@gmail.com>
2021-08-16 13:03:10 +0700

    editRequestDetail

--- src/app/shared/components/request-card-details/request-card-details.component.ts
+++ src/app/shared/components/request-card-details/request-card-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import {PageEvent} from '@angular/material/paginator';
+import { Component, OnInit, Input, Output, EventEmitter, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-request-card-details',
@@ -8,59 +9,60 @@ import {PageEvent} from '@angular/material/paginator';
 })
 export class RequestCardDetailsComponent implements OnInit {
   @Output() close = new EventEmitter();
-  @Input() request?: ISOSRequest = {};
-  constructor() { }
-  color = {
-    accent: 'accent',
-    primary: 'primary',
-    warn: 'warn',
-    basic: 'basic'
-  }
-  selectedColor = 'warn'
-  selectedIcon = "local_hospital"
-  icon = {
-    home: 'home',
-    menu: 'menu',
-    favorite: 'favorite',
-    sort: 'sort',
-    filter: 'filter',
-    hospital: 'local_hospital',
-    done: 'done',
-    alert: 'error',
-    fastfood: 'fastfood',
-    close: 'close'
+  lastestComment: { content: string; postTime: string; }[];
+  historyComment: { content: string; postTime: string; }[];
+  postList: ({ title: string; url: string; author: string; postTime: string; } | { title: string; author: string; postTime: string; url?: undefined; })[];
 
-  };
-  type = 'icon';
-  height = {
-    small: '32',
-    large: '40'
-  }
-  text = {
-    ratnguycap: 'Rất Nguy Cấp',
-    nguycap: 'Nguy Cấp',
-    daxacthuc: 'Đã Xác Thực',
-    yte: 'Y tế',
-    dothietyeu: 'Đồ Thiết Yếu'
+  constructor(public dialogRef: MatDialogRef<RequestCardDetailsComponent>,
+    @Inject(MAT_DIALOG_DATA) public request: ISOSRequest) {
+    this.lastestComment = [
+      {
+        content: 'Bệnh viện tạm thời không cần gì.',
+        postTime: '10:30 AM . Hôm nay',
+      },
+      {
+        content: 'Bệnh viện 100 may tho',
+        postTime: '10:30 AM . Hôm nay',
+      },
+    ];
+    this.historyComment = [
+      {
+        content: 'Bệnh viện tạm thời không cần gì.',
+        postTime: '10:30 AM . Hôm nay',
+      },
+    ];
+    this.postList = [
+      {
+        title: 'Hôm nay tại BV A.',
+        url: 'https://picsum.photos/300/200',
+        author: 'Hai Nguyen',
+        postTime: '10:30 AM . Hôm nay',
+      },
+      {
+        title: 'Đã hỗ trợ 1000 khẩu trang, 300 đồ bảo hộ',
+        author: 'Nguyễn Thị N . Nhóm thiện nguyện NTN',
+        postTime: '10:30 AM . Hôm nay',
+      },
+    ];
   }
   status = "Rất Nguy Cấp";
   typeRequest = "Y tế";
   onclose() {
     this.close.emit();
     console.log("close");
-  } 
-  length =0;
+  }
+  length = 0;
   pageSize = 1;
 
   // MatPaginator Output
-  pageEvent: PageEvent | undefined;
+  pageEvent: PageEvent = new PageEvent;
 
- 
+
   ngOnInit(): void {
-    if (this.request?.status != 'RẤT NGUY CẤP') { this.selectedColor = this.color.accent; this.status = "Nguy Cấp" }
+    if (this.request?.status != 'RẤT NGUY CẤP') { this.status = "Nguy Cấp" }
     if (this.request?.support_types?.length != 0) this.typeRequest = this.request?.support_types?.[0]?.name!;
-    this.length=this.request?.medias?.length!;
-    this.pageEvent!.pageIndex=0;
+    this.length = this.request?.medias?.length!;
+    this.pageEvent!.pageIndex = 0;
   }
 
 }
--- src/app/shared/services/rest-services/request-status.service.ts
+++ src/app/shared/services/rest-services/request-status.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class RequestStatusService {
+  static getRequestStatus(): string[] {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
   getRequestStatus(){
