cfa65450dd143824063f167b06e4c257b58190f2
Linh <linhnguyen@zentech.io>
2021-08-25 20:45:49 +0700

    rm

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
   host: 'http://54.69.39.96/api/v1',
-  googleApiKey: 'AIzaSyAnCQ9qgDE8waZ0zAPG-d-QPFSkfIgSH1Q',
+  googleApiKey: '-d-QPFSkfIgSH1Q',
   s3: {
-    AccessKeyId: 'AKIATKFI4GZN3O23YKCY',
-    Secret: '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'
+    AccessKeyId: '',
+    Secret: ''
   },
   mapStyle: [
     {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,10 +5,10 @@
 export const environment = {
   production: false,
   host: 'http://54.69.39.96/api/v1',
-  googleApiKey: 'AIzaSyAnCQ9qgDE8waZ0zAPG-d-QPFSkfIgSH1Q',
+  googleApiKey: '',
   s3: {
-    AccessKeyId: 'AKIATKFI4GZN3O23YKCY',
-    Secret: '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'
+    AccessKeyId: '',
+    Secret: ''
   },
   mapStyle: [
     {
